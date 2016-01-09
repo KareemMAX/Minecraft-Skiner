2950c827effd980036b679605c1ef0ac3d2a03ec
KareemMAX <kreem.morsy@hotmail.com>
2016-01-09 20:44:31 +0200

    Add ExSkinRes message

--- src/Minecraft skiner/ExSkinRes.vb
+++ src/Minecraft skiner/ExSkinRes.vb
@@ -1,3 +1,8 @@
 ﻿Public Class ExSkinRes
     Inherits Exception
+    Overloads ReadOnly Property Message
+        Get
+            Return "The skin resolution must be 64 x 64 or 64 x 32."
+        End Get
+    End Property
 End Class
